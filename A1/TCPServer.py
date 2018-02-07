0f19ddcb88b7ebfff6ffb862db0ca7a3276e3c7c
Connor <connor.fowlie@me.com>
2018-02-07 12:05:33 -0500

    TCPServer code

---
+++ A1/TCPServer.py
@@ -0,0 +1,13 @@
+from socket import *
+serverPort = 1200
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((‘’,serverPort))
+serverSocket.listen(1)
+print ‘The server is ready to receive’
+while 1:
+	connectionSocket, addr = serverSocket.accept()
+	message = connectionSocket.recv(2048)
+    pingRecievedTime = time.time()
+    returnMessage = (message, pingRecievedTime)
+	connectionSocket.send(returnMessage)
+	connectionSocket.close()

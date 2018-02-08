5dd345e1ee5671b8def6b72306440862ba4dcc4b
antoinekhouri <antoine.khouri@mail.mcgill.ca>
2018-02-08 16:12:37 -0500

    UDP code commented

--- A1/UDPClient.py
+++ A1/UDPClient.py
@@ -3,22 +3,22 @@ import sys
 from socket import *
 
 
-serverName = '142.157.20.166'
-serverPort = 1200
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.settimeout(1)
-
-for i in range (10):
-  pingSentTime = time.time()
-  messageID = "Ping ID: " + str(1000 + i)
-  clientSocket.sendto(messageID,(serverName, serverPort))
-  
+serverName = '142.157.20.166' //Server's IP Address
+serverPort = 1200             //Port used by server & client
+clientSocket = socket(AF_INET, SOCK_DGRAM)  //Initialize the client socket
+clientSocket.settimeout(1)    //Make the client wait no more than one second
+			      //before considering the message lost
+for i in range (10):          //Send 10 pings
+  pingSentTime = time.time()  //Record the time the ping was sent
+  messageID = "Ping ID: " + str(1000 + i) //Ping's ID
+  clientSocket.sendto(messageID,(serverName, serverPort))  //Send the message 
+  							   to the server
   try:
 
-    returnMessage, serverAddress = clientSocket.recvfrom(2048)
-    pongRecievedTime = time.time()
-    roundTripTime = pongRecievedTime-pingSentTime
-    pingRecievedTime = returnMessage.split(',')
+    returnMessage, serverAddress = clientSocket.recvfrom(2048) //Read server response & store it
+    pongRecievedTime = time.time()       		       //Record the time the server response was recieved
+    roundTripTime = pongRecievedTime-pingSentTime              //Calculate round trip time
+    pingRecievedTime = returnMessage.split(',')                //Split the return message to recover the timestamp sent by the server
     print ("--------------------------------------------")
     print ("")
     print ("Ping ID: ", messageID)
@@ -28,6 +28,6 @@ for i in range (10):
     print ("Round trip time is: ", roundTripTime)
     print ("")
 
-  except timeout:
+  except timeout:			//If the server response was larger than 1 second, assume the message was lost.
 
     print "Message Lost."
--- A1/UDPServer.py
+++ A1/UDPServer.py
@@ -2,14 +2,14 @@ import sys
 import time
 from socket import *
 
-serverPort = 1200 
-serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverPort = 1200 				//Port used by the client & server
+serverSocket = socket(AF_INET, SOCK_DGRAM) 	//Initialize server socket
 serverSocket.bind(('',serverPort))
 
 while 1:
 
-  message, clientAddress = serverSocket.recvfrom(2048)
-  pingRecievedTime = time.time()
-  returnMessage = message + ","+ str(pingRecievedTime)
-  serverSocket.sendto(returnMessage, clientAddress)
+  message, clientAddress = serverSocket.recvfrom(2048) 	//Recieve the client's ping
+  pingRecievedTime = time.time()			//Record the time at which the client's ping was recieved
+  returnMessage = message + ","+ str(pingRecievedTime)  //Set up the return message
+  serverSocket.sendto(returnMessage, clientAddress)     //Send the return message
 

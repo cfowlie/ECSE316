504b4fe9900bb80d47e467469918a9fab8f5f845
antoinekhouri <antoine.khouri@mail.mcgill.ca>
2018-02-08 16:27:05 -0500

    Update UDP Client to have all the required values

--- A1/UDPClient.py
+++ A1/UDPClient.py
@@ -3,22 +3,23 @@ import sys
 from socket import *
 
 
-serverName = '142.157.20.166' //Server's IP Address
-serverPort = 1200             //Port used by server & client
-clientSocket = socket(AF_INET, SOCK_DGRAM)  //Initialize the client socket
-clientSocket.settimeout(1)    //Make the client wait no more than one second
-			      //before considering the message lost
-for i in range (10):          //Send 10 pings
-  pingSentTime = time.time()  //Record the time the ping was sent
-  messageID = "Ping ID: " + str(1000 + i) //Ping's ID
-  clientSocket.sendto(messageID,(serverName, serverPort))  //Send the message 
-  							   to the server
+serverName = '142.157.21.102' #Server's IP Address
+serverPort = 1200             #Port used by server & client
+clientSocket = socket(AF_INET, SOCK_DGRAM)  #Initialize the client socket
+startTime = time.time()
+clientSocket.settimeout(1)    #Make the client wait no more than one second
+			      #before considering the message lost
+for i in range (10):          #Send 10 pings
+  pingSentTime = time.time()  #Record the time the ping was sent
+  messageID = "Ping ID: " + str(1000 + i) #Ping's ID
+  clientSocket.sendto(messageID,(serverName, serverPort))  #Send the message 
+  							   #to the server
   try:
 
-    returnMessage, serverAddress = clientSocket.recvfrom(2048) //Read server response & store it
-    pongRecievedTime = time.time()       		       //Record the time the server response was recieved
-    roundTripTime = pongRecievedTime-pingSentTime              //Calculate round trip time
-    pingRecievedTime = returnMessage.split(',')                //Split the return message to recover the timestamp sent by the server
+    returnMessage, serverAddress = clientSocket.recvfrom(2048) #Read server response & store it
+    pongRecievedTime = time.time()       		       #Record the time the server response was recieved
+    roundTripTime = pongRecievedTime-pingSentTime              #Calculate round trip time
+    pingRecievedTime = returnMessage.split(',')                #Split the return message to recover the timestamp sent by the server
     print ("--------------------------------------------")
     print ("")
     print ("Ping ID: ", messageID)
@@ -26,8 +27,11 @@ for i in range (10):          //Send 10 pings
     print ("Ping successfully recieved by server at : ",pingRecievedTime[1]) 
     print ("Pong successfully recieved at: ", pongRecievedTime)
     print ("Round trip time is: ", roundTripTime)
+    print ("It took ",(float(pingRecievedTime[1])-pingSentTime), " for the ping to reach the server")
+    print ("It took ",(pongRecievedTime-float(pingRecievedTime[1])), " for the corresponding pong to reach the client")
+    print ("The entire ping/pong message exchange took ", (pongRecievedTime-startTime))
     print ("")
 
-  except timeout:			//If the server response was larger than 1 second, assume the message was lost.
+  except timeout:			#If the server response was larger than 1 second, assume the message was lost.
 
     print "Message Lost."
--- A1/UDPServer.py
+++ A1/UDPServer.py
@@ -2,14 +2,14 @@ import sys
 import time
 from socket import *
 
-serverPort = 1200 				//Port used by the client & server
-serverSocket = socket(AF_INET, SOCK_DGRAM) 	//Initialize server socket
+serverPort = 1200 				#Port used by the client & server
+serverSocket = socket(AF_INET, SOCK_DGRAM) 	#Initialize server socket
 serverSocket.bind(('',serverPort))
 
 while 1:
-
-  message, clientAddress = serverSocket.recvfrom(2048) 	//Recieve the client's ping
-  pingRecievedTime = time.time()			//Record the time at which the client's ping was recieved
-  returnMessage = message + ","+ str(pingRecievedTime)  //Set up the return message
-  serverSocket.sendto(returnMessage, clientAddress)     //Send the return message
+  print ("Server ready")
+  message, clientAddress = serverSocket.recvfrom(2048) 	#Recieve the client's ping
+  pingRecievedTime = time.time()			#Record the time at which the client's ping was recieved
+  returnMessage = message + ","+ str(pingRecievedTime)  #Set up the return message
+  serverSocket.sendto(returnMessage, clientAddress)     #Send the return message
 

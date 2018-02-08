9570969f7d66291b6c8499ba78120a7b6bb58058
Connor <connor.fowlie@me.com>
2018-02-08 16:31:56 -0500

    Clean up and added comments

--- A1/TCPServer.py
+++ A1/TCPServer.py
@@ -2,14 +2,14 @@ import time
 import sys
 
 from socket import *
-serverPort = 1200
+serverPort = 1200 #Port used by the client & server
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print 'The server is ready to receive'
 while 1:
 	connectionSocket, addr = serverSocket.accept()
-	message = connectionSocket.recv(2048)
-	pingRecievedTime = time.time()
-	returnMessage =  message + ","+ str(pingRecievedTime)
-	connectionSocket.send(returnMessage)
+	message = connectionSocket.recv(2048) 					#Recieve the client's ping
+	pingRecievedTime = time.time()							#Record the time at which the client's ping was recieved
+	returnMessage =  message + ","+ str(pingRecievedTime)	#Prep return msg
+	connectionSocket.send(returnMessage)					#Send response
